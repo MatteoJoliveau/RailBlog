19f89089c3c5ead283313048219b719567b1d32d
Matteo Joliveau <matteojoliveau@outlook.com>
2016-07-03 10:39:56 +0000

    Implemented CRUD operations for posts

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20160617141935) do
 
-  create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
     t.text     "body",       limit: 65535
     t.datetime "created_at",               null: false
